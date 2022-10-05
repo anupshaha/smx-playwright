54be6a027dcc08943a6f5d307da6c45468a72c13
Anup Shaha <Anup.Shaha@nice.com>
2022-10-05 08:35:03 +0530

    Removed Temp Files

--- tests/E2E/DemoTest/jenkins.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-for (let i = 0; i < 10; i++) {
-  test.only(`basic test numer ${i}`, async ({ page }) => {
-    await page.waitForTimeout(1 * 750);
-    expect(true).toBe(true);
-  });
-};
